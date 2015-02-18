664c7021b883b63db6c63587a65997539421a280
Hannah Carney <hannahcarneyart@gmail.com>
2015-02-18 14:33:34 +0000

    starting putting in error for name too short

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,9 +9,13 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-  	Restaurant.create(restaurant_params)
-  	redirect_to '/restaurants'
-  end
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      render 'new'
+    end
+end
 
   def restaurant_params
     params.require(:restaurant).permit(:name)
